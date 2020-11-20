1a25fdb32cd60c00104ee02544765a2dd18da9bb
fabio.alves <fabio.alves@exablack.com>
2020-11-19 22:10:37 -0300

    initial commit

---
+++ src/main/java/com/aviato/Aviato/AviatoApplication.java
@@ -0,0 +1,13 @@
+package com.aviato.Aviato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AviatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AviatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aviato/Aviato/AviatoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aviato.Aviato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AviatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
